55a0ddde39977a860c0a00569ff1dcf0eefc54fa
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-04-13 17:17:49 +0900

    add group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,24 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.create(group_params)
   end
 
   def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+
+  end
+   private
+  def group_params
+    params.require(:group).permit(:group_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, only: %i(session registration)
+  resources :groups
 
   root 'chats#index'
 end
