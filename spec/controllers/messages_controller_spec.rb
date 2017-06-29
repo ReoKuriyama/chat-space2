b6410df09d363d503f85c7035463971b27954150
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-06-30 02:49:55 +0900

    test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,32 +1,37 @@
 require 'rails_helper'
 
 describe MessagesController, type: :controller do
+  let(:message) { create(:message) }
   let(:user) { create(:user) }
   let(:group) { create(:group) }
 
-  # userをcreateし、let内に格納
-
-  describe 'GET #index' do
+  describe 'logined' do
     before do
       login_user user
-      # controller_macros.rb内のlogin_userメソッドを呼び出し
     end
 
-    it "assigns the requested group to @group" do
-      get :index, params: {group_id: group}
-      expect(assigns(:group)).to eq group
-    end
+    describe 'GET #index' do
+      before do
+        get :index, params: {group_id: group}
+      end
 
-    it "renders the :index template" do
-      get :index, params: {group_id: group}
-      expect(response).to render_template :index
-    end
-  end
+      it "assigns the requested group to @message" do
+        message = Message.new
+        expect(assigns(:message)).to be_a_new Message
+      end
 
-  describe 'Get #create' do
-    before do
-      login_user user
-      # controller_macros.rb内のlogin_userメソッドを呼び出し
+      it "assigns the requested group to @group" do
+        expect(assigns(:group)).to eq group
+      end
+
+      it "assigns the requested groups to @groups" do
+        groups = user.groups
+        expect(assigns(:groups)).to match(groups)
+      end
+
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
     end
 
     describe 'GET #create' do
@@ -34,6 +39,11 @@ describe MessagesController, type: :controller do
         get :create, params: {group_id: group, message: {body: "sample"}}
         expect(response).to redirect_to( group_messages_path)
       end
+
+      it "saves the new contact in the database" do
+        expect{
+          post :create, params: {message: attributes_for(:message), group_id: group.id}}.to change(Message, :count).by(1)
+        end
 
       it 'message failed' do
         get :create, params: {group_id: group, message: {body: nil}}
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,5 +2,5 @@ module ControllerMacros
   def login_user(user)
       @request.env["devise.mapping"] = Devise.mappings[:user]
       sign_in user
-    end
+  end
 end
