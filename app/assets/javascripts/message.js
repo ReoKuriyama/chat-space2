7c866fde65f554c7378db9b41fd84b5187015c90
ReoKuriyama <gagabornthisway34@gmail.com>
2017-07-06 16:46:42 +0900

    asynchronous-communicatioin

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,31 @@
 $(function(){
-  $('#new-message').on('submit', function(e){
+  function buildHTML(message){
+   if (message.image) {
+      var ImageHTML = '<br><img src="' + message.image + '">';
+    } else {
+      var ImageHTML = '';
+    }
+    var html =
+      '<li>' +
+      '<div class="chat-body">' +
+      '<p class="chat-body__username">' +
+      message.name +
+      '</p>' +
+      '<p class="chat-body__time">' +
+      message.time +
+      '</p>' +
+      '</div>' +
+      '<p class="chat-body__message">' +
+      message.body +
+      ImageHTML +
+      '</p>' +
+      '</li>';
+    return html
+  }
+
+  $('form#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData('#new-message-form');
-    console.log(formData);
+    var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -14,8 +37,8 @@ $(function(){
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.todos').append(html);
-      textField.val('');
+      $('.wrap ul').append(html)
+      $('#message_body').val('')
     })
     .fail(function() {
       alert('error');
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
-json.name message.user.name
-json.time message.created_at
-json.image message.image
-json.body message.body
-json.image_url message.image.url
+json.name @message.user.name
+json.time @message.created_at
+json.image @message.image_url
+json.body @message.body
+
