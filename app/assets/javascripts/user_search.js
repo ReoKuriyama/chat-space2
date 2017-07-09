fd60fa76f8ee1e6bdd60241c31733faaa73add4d
ReoKuriyama <gagabornthisway34@gmail.com>
2017-07-09 22:02:17 +0900

    Revert "Revert "WIP""
    This reverts commit 20a4867238179fd210b962808cf9beee591a0dd0.

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,27 +1,61 @@
 $(function() {
-  function buildHTML(todo) {
+  function appendUser(user) {
     var html =
-    '<p>' +
-    'test'+
-    '</p>'
+    '<div class="chat-group-user clearfix">' +
+      '<p class="chat-group-user__name">' +
+      user.name +
+      '</p>' +
+      '<a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="' + user.id + '" data-user-name="' + user.name + '">追加' +
+      '</a>' +
+      '</div>';
     return html;
+  }
+
+  function addUser(id, name){
+    var html =
+      '<div class="chat-group-user clearfix" id="chat-group-user-' + id +  '">' +
+      '<input type="hidden" name="group[user_ids][]" value="' + id + '">' +
+      '<p class="chat-group-user__name">' +
+      name +
+      '</p>' +
+      '<a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="' + id + '">削除' +
+      '</a>' +
+      '</div>';
+    return html
   }
 
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input)
     $.ajax({
       type: 'GET',
       url: '/users',
-      data: { keyword: input },
+      data: {
+        name: input
+      },
       dataType: 'json'
     })
 
-    .done(function(products) {
-      console.log('success')
+    .done(function(users) {
+      var html = "";
+      users.forEach(function(user){
+        html += appendUser(user);
+      });
+
+      $('#user-search-result').html(html)
     })
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  // 検索結果に出てきたユーザーにイベントを設定
+  $('#user-search-result').on('click', '.user-search-add', function(){
+    var id = $(this).data('userId');
+    var name = $(this).data('userName');
+    var html = addUser(id, name);
+
+    $('#chat-group-users').append(html);
+
+    $(this).parent('.chat-group-user').remove();
   });
 });
